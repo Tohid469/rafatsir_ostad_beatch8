2643ad08db248c9e4606b2bbcd634a948dfe058b
Tohid <mrtohid0020@gmail.com>
2024-12-24 22:35:32 +0600

    comit module 13 class 2 (Integrate get product list and add new product apis)

--- lib/ui/screens/product_list_screen.dart
+++ lib/ui/screens/product_list_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:rafatsir_ostad_beatch8/models/product.dart';
 import 'package:rafatsir_ostad_beatch8/ui/screens/add_new_product_screen.dart';
 import 'package:rafatsir_ostad_beatch8/ui/widgets/product_item.dart';
 
@@ -10,17 +14,44 @@ class ProductListScreen extends StatefulWidget {
 }
 
 class _ProductListScreenState extends State<ProductListScreen> {
+  List<Product> productList = [];
+  bool _getProductListInProgress = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _getProductList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Product list'),
+        actions: [
+          IconButton(onPressed: () {
+            _getProductList();
+          }, icon: const Icon(Icons.refresh),),
+        ],
       ),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return const ProductItem();
+      body: RefreshIndicator(
+        onRefresh: () async {
+          _getProductList();
         },
+        child: Visibility(
+          visible: _getProductListInProgress == false,
+          replacement: const Center(
+            child: CircularProgressIndicator(),
+          ),
+          child: ListView.builder(
+            itemCount: productList.length,
+            itemBuilder: (context, index) {
+              return ProductItem(
+                product: productList[index],
+              );
+            },
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -29,5 +60,35 @@ class _ProductListScreenState extends State<ProductListScreen> {
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  Future<void> _getProductList() async {
+    productList.clear();
+    _getProductListInProgress = true;
+    setState(() {});
+    Uri uri = Uri.parse('https://crud.teamrabbil.com/api/v1/ReadProduct');
+    Response response = await get(uri);
+    print(response.statusCode);
+    print(response.body);
+    if (response.statusCode == 200) {
+      final decodedData = jsonDecode(response.body);
+      print(decodedData['status']);
+      for (Map<String, dynamic> p in decodedData['data']) {
+        Product product = Product(
+          id: p['_id'],
+          productName: p['ProductName'],
+          productCode: p['ProductCode'],
+          quantity: p['Qty'],
+          unitPrice: p['UnitPrice'],
+          image: p['Img'],
+          totalPrice: p['TotalPrice'],
+          createdDate: p['CreatedDate'],
+        );
+        productList.add(product);
+      }
+      setState(() {});
+    }
+    _getProductListInProgress = false;
+    setState(() {});
   }
 }
--- lib/ui/widgets/product_item.dart
+++ lib/ui/widgets/product_item.dart
@@ -1,30 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:rafatsir_ostad_beatch8/models/product.dart';
 import 'package:rafatsir_ostad_beatch8/ui/screens/update_product_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  const ProductItem({super.key});
+  const ProductItem({super.key, required this.product});
+
+  final Product product;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       leading: Image.network(
-          'https://adminapi.applegadgetsbd.com/storage/media/large/iPhone-14-Starlight-8954.jpg'),
-      title: const Text('Product name'),
-      subtitle: const Column(
+        product.image ?? '',
+        width: 40,
+      ),
+      title: Text(product.productName ?? 'Unknown'),
+      subtitle: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Product Code: #CODE'),
-          Text('Quantity: 12'),
-          Text('Price: 34535'),
-          Text('Total Price: 23232323'),
+          Text('Product Code: ${product.productCode ?? 'Unknown'}'),
+          Text('Quantity: ${product.quantity ?? 'Unknown'}'),
+          Text('Price: ${product.unitPrice ?? 'Unknown'}'),
+          Text('Total Price: ${product.totalPrice ?? 'Unknown'}'),
         ],
       ),
       trailing: Wrap(
         children: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.delete)),
+          IconButton(onPressed: () {
+            // TODO: implement delete product api
+          }, icon: const Icon(Icons.delete)),
           IconButton(
             onPressed: () {
-              Navigator.pushNamed(context, UpdateProductScreen.name);
+              Navigator.pushNamed(
+                context,
+                UpdateProductScreen.name,
+                arguments: product,
+              );
             },
             icon: const Icon(Icons.edit),
           ),
