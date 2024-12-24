8e4cba496250ed0f0f17b183d3b705a546397323
Tohid <mrtohid0020@gmail.com>
2024-12-24 20:49:17 +0600

    comit module 9 class 1 (Add Theme, Stack, Positioned)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 
-/// Navigation
-
-// Route
-// Route to Route Navigation
-// Navigator
-// Stack
+/// Theme, Stack, Positioned
+/// Code Smell
+/// Code Duplication
 
 void main() {
   runApp(HelloWorldApp());
@@ -18,6 +15,56 @@ class HelloWorldApp extends StatelessWidget {
       debugShowCheckedModeBanner: true,
       title: 'Hello World App',
       home: Home(),
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.pink,
+          foregroundColor: Colors.white,
+          titleTextStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          border:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.green)),
+          enabledBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.green)),
+          focusedBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.green)),
+        ),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.orange,
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.orange,
+          foregroundColor: Colors.white,
+          titleTextStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          border:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.pink)),
+          enabledBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.pink)),
+          focusedBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.pink)),
+        ),
+      ),
+      themeMode: ThemeMode.system,
     );
   }
 }
@@ -32,36 +79,43 @@ class Home extends StatelessWidget {
         title: Text('Home'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return Profile(username: 'Rabbi');
-                    },
-                  ),
-                );
-              },
-              child: Text('Go to profile'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return Settings();
-                    },
-                  ),
-                );
-              },
-              child: Text('Go to Settings'),
-            )
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(),
+              TextField(),
+              TextField(),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return Profile(username: 'Rabbi');
+                      },
+                    ),
+                  );
+                },
+                child: Text('Go to profile'),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return Settings();
+                      },
+                    ),
+                  );
+                },
+                child: Text('Go to Settings'),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -78,6 +132,7 @@ class Profile extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile'),
+        backgroundColor: Colors.green,
       ),
       body: Center(
         child: Column(
@@ -137,11 +192,38 @@ class Settings extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => Profile(username: 'Fahad',),
+                    builder: (context) => Profile(
+                      username: 'Fahad',
+                    ),
                   ),
                 );
               },
               child: Text('Move to profile'),
+            ),
+            Stack(
+              // alignment: Alignment.center,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 80,
+                  height: 80,
+                  color: Colors.yellow,
+                ),
+                Positioned(
+                  bottom: 2,
+                  right: 8,
+                  width: 100,
+                  child: Container(
+                    width: 60,
+                    height: 60,
+                    color: Colors.green,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
